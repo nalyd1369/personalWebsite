776212d6b08e105831947a2b3db66407e13308e4
nalyd1369 <nalyd1369@gmail.com>
2023-08-29 14:03:06 -0500

    Update website styling

---
+++ index.js
@@ -0,0 +1,67 @@
+const wrapper = document.getElementById("tiles");
+
+let columns = 0,
+    rows = 0,
+    toggled = false;
+
+const toggle = () => {
+  toggled = !toggled;
+  
+  document.body.classList.toggle("toggled");
+}
+
+const handleOnClick = index => {
+  toggle();
+  
+  anime({
+    targets: ".tile",
+    opacity: toggled ? 0 : 1,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index
+    })
+  });
+}
+
+const createTile = index => {
+  const tile = document.createElement("div");
+  
+  tile.classList.add("tile");
+  
+  tile.style.opacity = toggled ? 0 : 1;
+  
+  tile.onclick = e => handleOnClick(index);
+  
+  return tile;
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  });
+}
+
+const createGrid = () => {
+  wrapper.innerHTML = "";
+  
+  var size = 30;
+  if (wrapper.clientWidth > 3500) {
+    size = 120
+  } else if (wrapper.clientWidth > 2000) {
+    size = 70
+  } else if (wrapper.clientWidth > 800) {
+    size = 40
+  }
+  
+  columns = Math.floor(wrapper.clientWidth / size);
+  rows = Math.floor(wrapper.clientHeight / size);
+  
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
+  
+  createTiles(columns * rows);
+}
+
+createGrid();
+
+window.onresize = () => createGrid();
